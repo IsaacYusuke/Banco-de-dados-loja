87803d28a64639efbe1496380a44a04b0f2c297f
IsaacYusuke <isaacyusuke@usp.br>
2024-02-02 12:02:07 -0300

    html teste

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ItemViewSet
+from . import views
 
+
+#Pagina do REST API
 router = DefaultRouter()
 router.register(r'items', ItemViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('router', include(router.urls)),
+    path('', views.home, name='home'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,6 +5,11 @@ from .serializers import ItemSerializer
 
 # Create your views here.
 
+def home(request):
+    # seu código de lógica
+    return render(request, 'myapp/home.html', {'var1': 'valor1'})
+
+#Pagina do REST API
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer

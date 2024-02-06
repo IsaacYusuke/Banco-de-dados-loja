10ce0d34bc07755df345bd89d079e68c2d9e7bb3
IsaacYusuke <isaacyusuke@usp.br>
2024-02-06 13:08:10 -0300

    Pagina de admin personalizada com imagem

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.utils.html import format_html
 from .models import Item
 
-admin.site.register(Item)
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'descricao', 'preco', 'imagem']
+    list_display = ['nome', 'descricao', 'preco', 'image_tag']
+    readonly_fields = ['image_tag']
+
+    def image_tag(self, obj):
+        if obj.imagem:
+            return format_html('<img src="{}" height="150" width="auto" />', obj.imagem.url)
+        return "Sem imagem"
+    
+    image_tag.short_description = 'Imagem'
+
+
+# Register your models here.
+admin.site.register(Item, ItemAdmin)

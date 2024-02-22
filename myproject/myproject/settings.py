eeb1049901f34551b5fbe517fff3e05a2a24c35b
IsaacYusuke <isaacyusuke@usp.br>
2024-02-22 17:15:25 -0300

    Pillow no requirements.txt e settings pro react acessar a API

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'myapp',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Permitir acesso do front-end React
+    "https://meuappexemplo.com",  # Adicione outras origens conforme necessário
 ]
 
 ROOT_URLCONF = 'myproject.urls'
